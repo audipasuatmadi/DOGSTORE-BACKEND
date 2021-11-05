fd567c8f2cab6261801c6eb5b8fb60f6a245e767
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-11-05 10:12:07 +0800

    [#1] initialize project

---
+++ src/test/java/com/lordbetok/projects/dogstore/DogstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lordbetok.projects.dogstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
